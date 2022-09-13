04d7cc6e9e21e2e94d86c1bba16da277dcd22c5b
Piyush Negi <negip7740@gmail.com>
2022-09-13 17:59:49 +0530

    adding html

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
 /* Iteration 4: Make the Everything Work */
+//<!-- Michael Jackson - background, socks,shirt,pant,shoes,gloves,jacket,face,hat -->
 
 const MJbackground = document.getElementById('MJbackground');
 MJbackground.oninput = () =>{
     background.style.fill = MJbackground.value
+}
+
+const MJsocks = document.getElementById('MJsocks');
+MJsocks.oninput = () =>{
+    socks.style.fill = MJsocks.value
+}
+
+const MJshirt = document.getElementById('MJshirt');
+MJshirt.oninput = () =>{
+    shirt.style.fill = MJshirt.value
+}
+
+const MJpant = document.getElementById('MJpant');
+MJpant.oninput = () =>{
+    pant.style.fill = MJpant.value
+}
+
+const MJshoes = document.getElementById('MJshoes');
+MJshoes.oninput = () =>{
+    shoes.style.fill = MJshoes.value
+}
+
+const MJgloves = document.getElementById('MJgloves');
+MJgloves.oninput = () =>{
+    gloves.style.fill = MJgloves.value
+}
+
+const MJjacket = document.getElementById('MJjacket');
+MJjacket.oninput = () =>{
+    jacket.style.fill = MJjacket.value
+}
+
+const MJface = document.getElementById('MJface');
+MJface.oninput = () =>{
+    face.style.fill = MJface.value
+}
+
+const MJhat = document.getElementById('MJhat');
+MJhat.oninput = () =>{
+    hat.style.fill = MJhat.value
 }
