fccf28ec475eb00ed45ad60a01f448e527a82109
Piyush Negi <negip7740@gmail.com>
2022-09-13 17:56:15 +0530

    adding html

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 /* Iteration 4: Make the Everything Work */
+
+const MJbackground = document.getElementById('MJbackground');
+MJbackground.oninput = () =>{
+    background.style.fill = MJbackground.value
+}
